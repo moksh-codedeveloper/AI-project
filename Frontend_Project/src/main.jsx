23d9cfcdccb7e61a2c1e767ffd135fa6297cf116
Moksh Malde <maldemoksh437@gmail.com>
2024-12-26 23:02:47 +0530

    Add all the essentia; files and folders

--- Frontend_Project/src/main.jsx
+++ Frontend_Project/src/main.jsx
@@ -1,10 +1,55 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, createBrowserRouter } from "react-router-dom";
+import Signin from "./auth/Signin.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import CreateResume from "./components/Resume/CreateResume.jsx";
+import Display from "./components/Resume/Display.jsx";
+import Delete from "./components/Resume/Delete.jsx";
+import Update from "./components/Resume/Update.jsx";
+import Signup from "./auth/Signup.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/resume",
+        element: <Display />,
+        children: [
+          {
+            path: "/createResume",
+            element: <CreateResume />,
+          },
+          {
+            path: "delete",
+            element: <Delete />,
+          },
+          {
+            path: "/update",
+            element: <Update/>
+          }
+        ],
+      },
+    ],
+  },
+  {
+    path: "/auth/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/auth/sigin",
+    element: <Signin />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter router={router} />
+  </StrictMode>
+);
