7eca41b9af3bedc554b8737347631deda38c1663
Moksh Malde <maldemoksh437@gmail.com>
2024-12-27 10:44:36 +0530

    Add a import and file for components

--- Frontend_Project/src/components/index.js
+++ Frontend_Project/src/components/index.js
@@ -4,12 +4,23 @@ import {Footer} from "./Footer.jsx"
 import {Container} from "./Container.jsx"
 import {Dashboard} from "./Dashboard.jsx"
 import {Input} from "./Input.jsx"
-
+import {CreateResume} from './Resume/CreateResume.jsx'
+import {Update} from "./Resume/Update.jsx"
+import {Display} from "./Resume/Update.jsx"
+import {Delete} from "./Resume/Delete.jsx"
+import {LinkedSkill} from './Features/LinkedSkill.jsx'
+import {VoiceToText} from './Features/VoiceToText.jsx'
 export {
     Button,
     Header,
     Footer,
     Container,
     Dashboard,
-    Input
+    Input,
+    CreateResume,
+    Update,
+    Display,
+    Delete,
+    LinkedSkill,
+    VoiceToText
 }
